846c769225b25a4a939eb67a9d2378a5295f9d13
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2023-03-08 22:35:34 +0000

    Made it multi-dimensional
    The Momentum transfer now works in when it is taking in two vectors and not just for linear velocities allowing for it to work for my newton's cradle

--- MomentumExample.py
+++ MomentumExample.py
@@ -1,19 +1,24 @@
 import numpy as np
+from Classes import Particle
+
+
 def MomentumTransfer():
-    m1=1
-    m2=1
-    u1=2.5
-    u2=0
-    mu1=m1/2
-    mu2=m2/2
-    mr=m2/m1
-    a=(mu2+mu1*mr**2)
-    b=(-2*mr*mu1*u1-2*u2*(mr**2)*mu1*u2*mr**2)
-    c=2*mr*u1*mu1*u2+mu1*(mr**2)*(u2**2)-mu2*u2
-    x=(-b+np.sqrt((b**2)-4*a*c))/2*a
-    y=(-b-np.sqrt((b**2)-4*a*c))/2*a
-    o=u1+mr*(u2-x)
-    p=u1+mr*(u2-y)
-    return (x,o),(y,p)
+    sam=np.array([2.5, 0, 0])
+    hed=np.array([0, 0, 0])
+    for j in range(3):
+        m1=1 #Mass of Fist ball in Kg
+        m2=1 #Mass of Second ball in Kg
+        u1= sam[j]#Initial Velocity of first ball in m/s
+        u2=hed[j]#Initial Velocity of second ball in m/s
+        mu1=m1/2 #Half mass used in Kinetic energy EQN
+        mu2=m2/2 #Half mass used in Kinetic energy EQN
+        mr=m2/m1 #Ratio of the two masses
+        a=(mu2+mu1*mr**2)
+        b=(-2*mr*mu1*u1-2*u2*(mr**2)*mu1*u2*mr**2)
+        c=2*mr*u1*mu1*u2+mu1*(mr**2)*(u2**2)-mu2*u2
+        hed[j]=(-b+np.sqrt((b**2)-4*a*c))/2*a #Quadratic forumla
+        sam[j]=u1+mr*(u2-hed[j])
+
+    return (sam,hed)
 print (MomentumTransfer())
 
