8a10a2d179df861df767c14993d3d285c0dacc37
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2023-03-08 23:55:21 +0000

    changed test conditions

--- Test[1].py
+++ Test[1].py
@@ -6,7 +6,7 @@ pendulum = Pendulum(length=1, mass=1)
 
 # Set the time step and total time for the simulation
 deltaT = 0.01
-total_time = 10
+total_time = 100
 
 # Create empty arrays to store the time and position data
 time_data = np.arange(0, total_time, deltaT)
