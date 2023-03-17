ea0b46411fba318c96e03df5f5aed6fc6e4b8d3e
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2023-03-17 21:45:07 +0000

    Changed to plot Total Position
    Added this code to make the plot of the magnitude of the position vector as well as the magnitudes of the 3 dimensions. This should be conserved at 10 metres seeing as we are using a light inextensible rod

--- Test[1].py
+++ Test[1].py
@@ -2,26 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Classes import Pendulum, Particle
 
-pendulum = Pendulum(length=10, mass=1, radius=2.5, psi=np.pi/8)
+pendulum = Pendulum(length=10, mass=1, radius=2.5, psi=np.pi/12)
 
 # Simulate the pendulum movement
 dt = 0.1
-x=100000
-t = np.arange(0, x, dt)
+x=1000
+t = np.arange(-1, x, dt)
 num_steps = len(t)
 pos = np.zeros((num_steps, 3))
+pos_mag = np.zeros(num_steps)
 
 for particle in pendulum.particles_list:
     for i in range(num_steps):
         pendulum.movement()
         particle.EulerCromer(dt)
         pos[i] = particle.position
+        pos_mag[i] = np.linalg.norm(particle.position)
         print("position = {}".format(particle.position))
         print("Velocity = {}".format(particle.velocity))
         print("Acceleration = {}".format(particle.acceleration))
 
-# Plot x, y, and z components of position separately
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True, figsize=(8, 8))
+# Plot x, y, z components, and magnitude of position
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, sharex=True, figsize=(8, 10))
 fig.suptitle('Position of First Particle in Pendulum vs Time')
 
 ax1.plot(t, pos[:, 0], label='x')
@@ -34,7 +36,11 @@ ax2.set_ylabel('Position (m)')
 
 ax3.plot(t, pos[:, 2], label='z')
 ax3.legend()
-ax3.set_xlabel('Time (s)')
 ax3.set_ylabel('Position (m)')
+
+ax4.plot(t, pos_mag, label='magnitude')
+ax4.legend()
+ax4.set_xlabel('Time (s)')
+ax4.set_ylabel('Position (m)')
 
 plt.show()
