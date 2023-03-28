9e2420b742f6f68d6853d79fccfccea23e3cb01a
Sam Hedges <106534363+HedgeHogNo5@users.noreply.github.com>
2023-03-28 17:57:13 +0100

    Ammended the code
    Changed the code around, so it will now take in a number of particles and will plot the acceleration, velocity and positioning of those particles

--- Test[4].py
+++ Test[4].py
@@ -1,60 +1,94 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from Classes import Pendulum, Particle, NewtonCradle
+from Classes import NewtonsCradle, Particle
 
-cradle = NewtonCradle(num_particles=5, length=10, mass=1, radius=2.5, psi=np.pi/12)
+# Define the parameters of the Newton's Cradle
+num_particles = 5
+length = 10
+mass = 1
+radius = 2.5
 
-# Simulate the Newton's Cradle movement
+# Create the pendulum object with the specified number of particles
+Particle_1 = Particle(
+    position=np.array([0, 0, 0], dtype=float),
+    velocity=np.array([0, 0, 0], dtype=float),
+    acceleration=np.array([0, 0, 0], dtype=float),
+    name="Particle 1",
+    mass=1,
+    radius=2.5
+)
+Particle_2 = Particle(
+    position=np.array([0, 0, 0], dtype=float),
+    velocity=np.array([0, 0, 0], dtype=float),
+    acceleration=np.array([0, 0, 0], dtype=float),
+    name="Particle 2",
+    mass=1,
+    radius=2.5
+)
+pl = [Particle_1, Particle_2]
+newtonscradle = NewtonsCradle(particles_list=pl, length=10, psi=np.pi/12)
+
+
+# Simulate the movement of the Newton's Cradle
 dt = 0.00001
-x = 2*(cradle.Period())
+x = 2*(newtonscradle.Period())
 t = np.arange(0, x, dt)
 
 num_steps = len(t)
-pos = np.zeros((num_steps, 3*cradle.num_particles))
-pos_mag = np.zeros(num_steps*cradle.num_particles)
-vel = np.zeros((num_steps, 3*cradle.num_particles))
-vel_mag = np.zeros(num_steps*cradle.num_particles)
-accel = np.zeros((num_steps, 3*cradle.num_particles))
-accel_mag = np.zeros(num_steps*cradle.num_particles)
+pos = np.zeros((num_particles, num_steps, 3))
+pos_mag = np.zeros((num_particles, num_steps))
+vel = np.zeros((num_particles, num_steps, 3))
+vel_mag = np.zeros((num_particles, num_steps))
+accel = np.zeros((num_particles, num_steps, 3))
+accel_mag = np.zeros((num_particles, num_steps))
 
 for i in range(num_steps):
-    cradle.movement()
-    cradle.FOTIerror()
-    for j, particle in enumerate(cradle.particles_list):
+    newtonscradle.movement()
+    newtonscradle.FOTIerror()
+    for j, particle in enumerate(newtonscradle.particles_list):
         particle.Euler(dt)
-        pos[i, 3*j:3*j+3] = particle.position
-        pos_mag[i*cradle.num_particles+j] = np.linalg.norm(particle.position)
-        vel[i, 3*j:3*j+3] = particle.velocity
-        vel_mag[i*cradle.num_particles+j] = np.linalg.norm(particle.velocity)
-        accel[i, 3*j:3*j+3] = particle.acceleration
-        accel_mag[i*cradle.num_particles+j] = np.linalg.norm(particle.acceleration)
+        pos[j, i] = particle.position
+        pos_mag[j, i] = np.linalg.norm(particle.position)
+        vel[j, i] = particle.velocity
+        vel_mag[j, i] = np.linalg.norm(particle.velocity)
+        accel[j, i] = particle.acceleration
+        accel_mag[j, i] = np.linalg.norm(particle.acceleration)
 
-# Plot x, y, z components, and magnitude of position, velocity, and acceleration for each particle
-fig, axs = plt.subplots(cradle.num_particles, 3, sharex=True, figsize=(12, 8*cradle.num_particles))
-fig.suptitle('Position, Velocity, and Acceleration of Particles in Newton\'s Cradle vs Time')
+# Plot the position, velocity, and acceleration of each particle
+fig, axs = plt.subplots(num_particles, 3, sharex=True, figsize=(12, 8*num_particles))
+fig.suptitle('Position, Velocity, and Acceleration of Newton\'s Cradle Particles vs Time')
 
-for j in range(cradle.num_particles):
-    axs[j, 0].plot(t, pos[:, 3*j], label='x')
+for j in range(num_particles):
+    axs[j, 0].plot(t, pos[j, :, 0], label='x')
     axs[j, 0].legend()
-    axs[j, 0].set_ylabel(f'Particle {j+1} Position (m)')
+    axs[j, 0].set_ylabel('Position (m)')
 
-    axs[j, 1].plot(t, pos[:, 3*j+1], label='y')
+    axs[j, 1].plot(t, vel[j, :, 0], label='x')
     axs[j, 1].legend()
-    axs[j, 1].set_ylabel(f'Particle {j+1} Position (m)')
+    axs[j, 1].set_ylabel('Velocity (m/s)')
 
-    axs[j, 2].plot(t, pos[:, 3*j+2], label='z')
+    axs[j, 2].plot(t, accel[j, :, 0], label='x')
     axs[j, 2].legend()
-    axs[j, 2].set_ylabel(f'Particle {j+1} Position (m)')
+    axs[j, 2].set_ylabel('Acceleration (m/s^2)')
 
 plt.tight_layout()
 
-# Plot magnitude of position, velocity, and acceleration for each particle
-fig, axs = plt.subplots(cradle.num_particles, 3, sharex=True, figsize=(12, 8*cradle.num_particles))
+# Plot the magnitude of position, velocity, and acceleration of each particle
+fig, axs = plt.subplots(num_particles, 3, sharex=True, figsize=(12, 8*num_particles))
+fig.suptitle('Magnitude of Position, Velocity, and Acceleration of Newton\'s Cradle Particles vs Time')
 
-for j in range(cradle.num_particles):
-    axs[j, 0].plot(t, pos_mag[j*num_steps:(j+1)*num_steps], label='Position')
+for j in range(num_particles):
+    axs[j, 0].plot(t, pos_mag[j, :], label='Position')
     axs[j, 0].legend()
-    axs[j, 0].set_ylabel(f'Particle {j+1} Position Magnitude (m)')
+    axs[j, 0].set_ylabel('Position Magnitude (m)')
 
-    axs[j, 1].plot(t, vel_mag[j*num_steps:(j+1)*num_steps], label='Velocity')
-    axs[j,
+    axs[j, 1].plot(t, vel_mag[j, :], label='Velocity')
+    axs[j, 1].legend()
+    axs[j, 1].set_ylabel('Velocity Magnitude (m/s)')
+
+    axs[j, 2].plot(t, accel_mag[j, :], label='Acceleration')
+    axs[j, 2].legend()
+    axs[j, 2].set_ylabel('Acceleration Magnitude (m/s^2)')
+
+plt.tight_layout()
+plt.show()
